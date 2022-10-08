d79e837ff49f4392af76dd3aee180c86b0958294
junji oishi <abdaqoopmhm@gmail.com>
2022-10-08 21:50:07 +0900

    react-user-management-app
    ログインページの実装②

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useCallback, useState } from "react"
+import { useNavigate } from "react-router-dom";
+import { User } from "../types/api/user";
+
+export const useAuth = () => {
+    const navigate = useNavigate();
+
+    const [loading, setLoading] = useState(false);
+
+    const login = useCallback((id: string) => {
+        setLoading(true);
+
+        axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(res => {
+            if(res.data) {
+                navigate("home");
+            } else {
+                alert("ユーザーが見つかりません");
+            }
+        })
+        .catch(() => alert("ログインできません"))
+        .finally(() => setLoading(false));
+    }, [navigate]);
+    return{ login, loading }
+}
